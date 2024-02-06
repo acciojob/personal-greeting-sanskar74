6077b4dc2d57de44619ea7be717ec5f7960e2cab
sanskarsahu <sanskarsahu1580@gmail.com>
2024-02-06 16:16:48 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [inpval, setInpval] = useState("");
+
+  function handleInput(event) {
+    setInpval(event.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input type="text" value={inpval} onChange={handleInput}></input>
+      {inpval && <p>Hello {inpval}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
